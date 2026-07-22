6ca171cf77c9cc8ab7312b7956d9a1ec27745059
NikoMyoji <nicolas.vlte@gmail.com>
2026-07-22 18:48:54 +0200

    Basics spore growth

--- Assets/Scripts/Farm/Crops/Mushroom.cs
+++ Assets/Scripts/Farm/Crops/Mushroom.cs
@@ -1,5 +1,7 @@
+using NUnit.Framework;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -8,21 +10,42 @@ namespace FarmJam2026
     public class Mushroom : MonoBehaviour
     {
         [SerializeField]
-        private int _growthTime = 5;
+        private float _lifeTime = 15f;
+        [SerializeField]
+        private float _growthTime = 5f;
+        [SerializeField]
+        private float _sporeGrowthTime = 5f;
+        [SerializeField]
+        private int _harvestValue = 2;
+        [SerializeField]
+        private List<GameObject> _possiblesSporesPrefabs = new List<GameObject>();
+        [SerializeField]
+        private List<Transform> _sporeSlots = new List<Transform>();
+
         [SerializeField]
         private float _scale = 2.0f;
         [SerializeField]
         private Color _adultColor;
+
+        private Queue<Spore> _currentSpores = new Queue<Spore>();
+        private float _currentLifeTime = 0f;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             StartCoroutine(Grow(_scale, _growthTime));
+            _currentLifeTime = 0f;
+            Debug.Log($"Mushroom {gameObject.name} started growing");
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            _currentLifeTime += Time.deltaTime;
+            
+            if (_currentLifeTime >= _lifeTime)
+            {
+                Decay();
+            }
         }
 
         private IEnumerator Grow(float scale, float duration)
@@ -43,7 +66,45 @@ namespace FarmJam2026
             {
                 spriteRenderer.color = _adultColor;
             }
+            for (int i = 0; i< _harvestValue; i++)
+            {  
+                StartGrowSpore();
+            }
+        }
 
+        private void StartGrowSpore()
+        {
+            Debug.Log($"Mushroom {gameObject.name} started growing Spore");
+            GameObject sporePrefab = GameObject.Instantiate(_possiblesSporesPrefabs[UnityEngine.Random.Range(0, _possiblesSporesPrefabs.Count)],
+                                                            _sporeSlots[_currentSpores.Count].position, Quaternion.identity, _sporeSlots[_currentSpores.Count]);
+            Spore spore = sporePrefab.GetComponent<Spore>();
+            spore.InitSpore(_sporeGrowthTime);
+            _currentSpores.Enqueue(spore);
+        }
+        public Spore Harvest()
+        {
+            if (_currentSpores.Peek().HasGrown)
+            {
+                Spore spore = _currentSpores.Dequeue();
+                Debug.Log("Spore harvested");
+                Destroy(spore.gameObject);
+                StartGrowSpore();
+                return spore;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private void Decay()
+        {
+            Debug.Log($"Mushroom {gameObject.name} decayed");
+            while (_currentSpores.Count > 0)
+            {
+                Spore spore = _currentSpores.Dequeue();
+                Destroy(spore.gameObject);
+            }
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Scripts/Farm/Crops/Spore.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+namespace FarmJam2026
+{
+    [Serializable]
+    public class Spore : MonoBehaviour
+    {
+
+        private float _growthTime = 5f;
+
+        public bool HasGrown { get; private set; } = false;
+
+       
+
+        private void Update()
+        {
+            
+        }
+
+        public void InitSpore(float growthTime)
+        {
+            Debug.Log("Spore started growing");
+            _growthTime = growthTime;
+            StartCoroutine(Grow());
+        }
+        private IEnumerator Grow()
+        {
+            float time = 0;
+            while (time < _growthTime)
+            {
+                transform.localScale = Vector2.Lerp(Vector2.zero, Vector2.one, time / _growthTime);
+                time += Time.deltaTime;
+                yield return null;
+            }
+            transform.localScale = Vector2.one;
+            HasGrown = true;
+        }
+
+        public void  SetGrowthTime (int growthTime)
+        {
+            _growthTime = growthTime;
+        }
+
+
+
+    }
+}
