fd39857ea50178e31a483b5ec833dac36c1ac365
NikoMyoji <nicolas.vlte@gmail.com>
2026-07-22 16:21:46 +0200

    basic farm mechanics (plant)

---
+++ Assets/Scripts/Farm/FarmHandler.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+namespace FarmJam2026
+{
+    public class FarmHandler : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+     
+    }
+}
