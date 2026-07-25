3f2fee65bd2961cc51005a402a3b0fa92ae6b10a
Fares <fares.belmadi@gmail.com>
2026-07-25 21:25:18 +0800

    Ajout de la liste des spores dans le script inventaire (oublie)

--- Assets/Scripts/Inventaire/Inventaire.cs
+++ Assets/Scripts/Inventaire/Inventaire.cs
@@ -8,16 +8,20 @@ namespace FarmJam2026
     {
         public GameObject SporeInventaire;
         public GameObject GridInventaire;
+        List<GameObject> SporeInInventaire;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             EventManager.m_harvest.AddListener(AddSporesToInv);
+            SporeInInventaire = new List<GameObject>();
         }
 
         void AddSporesToInv(List<Spore> toAdd)
         {
             GameObject instanceSporeInventaire = Instantiate(SporeInventaire, GridInventaire.transform);
             instanceSporeInventaire.GetComponent<InventaireItem>().UpdateCount(toAdd.Count);
+            SporeInInventaire.Add(instanceSporeInventaire);
         }
 
         public void StartPlanting(Spore toPlant)
