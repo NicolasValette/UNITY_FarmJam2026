1082109a1b58f72125fc18ee400d6b6ad32b8404
Nono Geulin <veryblueviolin@gmail.com>
2026-07-28 00:48:24 +0200

    Blender

---
+++ Assets/Scripts/Genetics/Blender/Blender.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FarmJam2026
+{
+    public class Blender : MonoBehaviour
+    {
+        public List<GenomeData> Content { get; private set; } = new List<GenomeData>();
+    }
+}
