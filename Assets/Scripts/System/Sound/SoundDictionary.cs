6b30442ef964d0f46837f460aa351b9922807b67
Nono Geulin <veryblueviolin@gmail.com>
2026-08-02 23:05:53 +0200

    Editor code in editor sections

--- Assets/Scripts/System/Sound/SoundDictionary.cs
+++ Assets/Scripts/System/Sound/SoundDictionary.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace FarmJam2026
 {
@@ -17,14 +20,18 @@ namespace FarmJam2026
             var vals = Enum.GetValues(typeof(T));
             if (Audios.Count != vals.Length)
             {
+#if UNITY_EDITOR
                 Undo.RecordObject(this, $"Validate sound dico");
+#endif
                 var mem = new List<AudioClip>(Audios);
                 Audios = new List<AudioClip>(Audios);
                 for (int i = 0; i < vals.Length; i++)
                 {
                     Audios[i] = mem.Count > i ? mem[i] : null;
                 }
+#if UNITY_EDITOR
                 EditorUtility.SetDirty(this);
+#endif
             }
         }
     }
