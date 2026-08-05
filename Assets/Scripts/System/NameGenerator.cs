3437ba8919c51c726c2ba716fd2098fd2204653e
Nono Geulin <veryblueviolin@gmail.com>
2026-08-05 11:17:11 +0200

    Word dicos and generation

--- Assets/Scripts/System/NameGenerator.cs
+++ Assets/Scripts/System/NameGenerator.cs
@@ -1,9 +1,30 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace FarmJam2026
 {
     public class NameGenerator : MonoBehaviour
     {
+        public static NameGenerator Instance { get; private set;  }
+        public void OnEnable()
+        {
+            Instance = this;
+        }
 
+        [SerializeField]
+        public List<string> NameParts = new List<string>();
+
+        [SerializeField]
+        public List<string> Suffixes = new List<string>();
+
+        public string GenerateRandomName()
+        {
+            var name1 = Random.Range(0, NameParts.Count);
+            var name2 = Random.Range(0, NameParts.Count);
+            var suffix1 = Random.Range(0, Suffixes.Count);
+            var suffix2 = Random.Range(0, Suffixes.Count);
+            return $"{name1}{suffix1} {name2}{suffix2}";
+        }
     }
 }
