d4be7556be6061a14f69dfa51d461d67a92c76e2
Nono Geulin <veryblueviolin@gmail.com>
2026-08-06 15:27:16 +0200

    Create some test data

---
+++ Assets/Scripts/Genetics/Genes/VariantGene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace FarmJam2026.Assets.Scripts.Genetics.Genes
+{
+    public class NewMonoBehaviour : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
