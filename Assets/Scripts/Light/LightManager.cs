c23b5785983c96573f12f8298c46c0e3fe0c9bdf
NikoMyoji <nicolas.vlte@gmail.com>
2026-08-07 23:50:17 +0200

    Add lighning effect and firefly

--- Assets/Scripts/Light/LightManager.cs
+++ Assets/Scripts/Light/LightManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
@@ -19,7 +20,11 @@ namespace FarmJam2026
         private float _nightIntensity; 
         [SerializeField]
         private Color _nightColor;
+        [Space]
+        [SerializeField]
+        private float _switchDuration = 5f;
 
+        
         private void OnEnable()
         {
             EventManager.StartListening(EventManager.Events.OnStartDay, () => ChangeLightingSetting(true));
@@ -29,20 +34,38 @@ namespace FarmJam2026
         {
             EventManager.StopListening(EventManager.Events.OnStartDay, () => ChangeLightingSetting(true));
             EventManager.StopListening(EventManager.Events.OnStartNight, () => ChangeLightingSetting(false));
+           
+           
         }
 
         public void ChangeLightingSetting(bool _isDayTime)
         {
             if (_isDayTime)
             {
-                _globalLight.intensity = _dayIntensity;
-                _globalLight.color = _dayColor;
+                //_globalLight.intensity = _dayIntensity;
+                //_globalLight.color = _dayColor;
+                StartCoroutine(Switch(_nightColor, _dayColor, _nightIntensity, _dayIntensity));
             }
             else
             {
-                _globalLight.intensity = _nightIntensity;
-                _globalLight.color = _nightColor;
+                //_globalLight.intensity = _nightIntensity;
+                //_globalLight.color = _nightColor;
+                StartCoroutine(Switch(_dayColor, _nightColor, _dayIntensity, _nightIntensity));
             }
         }
+        private IEnumerator Switch(Color startingColor, Color targetColor, float startingIntensity, float targetIntensity)
+        {
+            float time = 0;
+            while (time < _switchDuration)
+            {
+                _globalLight.color = Color.Lerp(startingColor, targetColor, time / _switchDuration);
+                _globalLight.intensity = Mathf.Lerp(startingIntensity, targetIntensity, time / _switchDuration);
+                time += Time.deltaTime;
+                yield return null;
+            }
+            _globalLight.color = targetColor;
+            _globalLight.intensity = targetIntensity;
+        }
+
     }
 }
