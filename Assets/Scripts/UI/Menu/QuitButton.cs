234b54334c64db69eb0261ee0c159141c98a7df0
NikoMyoji <nicolas.vlte@gmail.com>
2026-08-17 14:47:18 +0200

    Remove Quit button on webgl build

--- Assets/Scripts/UI/Menu/QuitButton.cs
+++ Assets/Scripts/UI/Menu/QuitButton.cs
@@ -5,7 +5,16 @@ namespace FarmJam2026
 {
     public class QuitButton : MonoBehaviour
     {
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
+        {
+#if WINBUILD
+            gameObject.SetActive(true);
+#else
+            gameObject.SetActive(false);
+#endif
+        }
         public void QuitGame()
         {
 #if UNITY_EDITOR
